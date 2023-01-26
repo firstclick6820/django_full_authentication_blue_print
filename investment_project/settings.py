d96e05384779f66cfb5c763c27b761c8e4aa28ab
Khalid Momand <mohammadkhalidmomand@gmail.com>
2023-01-26 10:09:14 +0400

    Allowed Host is added

--- investment_project/settings.py
+++ investment_project/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['supervoter.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
