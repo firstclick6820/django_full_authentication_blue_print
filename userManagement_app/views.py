6c9f3d7e60839ec28e101b9b22bba3100bc51559
Khalid Momand <mohammadkhalidmomand@gmail.com>
2023-01-26 11:26:07 +0400

    new changes done

--- userManagement_app/views.py
+++ userManagement_app/views.py
@@ -114,13 +114,13 @@ def userRegister(request):
             email_template_name = "userManagement_app/Auth/patterns/email_verification_email.txt"
             c = {
 					"email":user.email,
-					'domain':'127.0.0.1:8000',
+					'domain':'supervoter.onrender.com',
 					'site_name': 'MKM IT SOLUTION',
 					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
 					"user": user,
                     'name':user.username ,
 					'protocol': 'http',
-                    'link': 'http://localhost:8000/accounts/email_verify?token={}'.format(token)
+                    'link': 'http://supervoter.onrender.com/accounts/email_verify?token={}'.format(token)
 					}
                     
                     
@@ -191,7 +191,7 @@ def user_password_reset(request):
                     email_template_name = "userManagement_app/Auth/patterns/password_reset_email.txt"
                     c = {
 					"email":user.email,
-					'domain':'127.0.0.1:8000',
+					'domain':'supervoter.onrender.com',
 					'site_name': 'MKM IT SOLUTION',
 					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
 					"user": user,
